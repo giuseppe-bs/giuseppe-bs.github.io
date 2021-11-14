6d1e3e85289904173da0a467a8fa5933fc102882
giuseppe-bs <giuseppescassiotti@hotmail.com>
2021-11-14 16:42:44 -0300

    icon test

--- scripts/index.js
+++ scripts/index.js
@@ -10,4 +10,25 @@ function r(response){
 
 function creatD(obj){
     console.log(obj)
+    obj.forEach(element => {
+        console.log(element)
+        var divProj = document.createElement("div");
+        var img = document.createElement("img");
+        img.setAttribute("src",element.img);
+        img.setAttribute("id","projImg");
+        var divinfo = document.createElement("div");
+        var divTitle = document.createElement("div");
+        var divlang = document.createElement("div");
+        var divText = document.createElement("div");
+        var tex = document.createTextNode(element.text);
+        divinfo.appendChild(divTitle)
+        divinfo.appendChild(divlang)
+        divinfo.appendChild(divText.appendChild(tex))
+        divProj.appendChild(img)
+        divProj.appendChild(divinfo)
+        divProj.id = "proj"
+        document.getElementById("projArea").appendChild(divProj)
+
+        //document.body.insertBefore(divProj,divAtual)
+    });
 }
